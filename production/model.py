9a76e3223e80ce340ca0330a06ef8277d5e37761
SonaAnna <baby-sa@ulster.ac.uk>
2024-12-09 23:39:36 +0000

    model code updated

--- production/model.py
+++ production/model.py
@@ -2,7 +2,7 @@
 import os
 import numpy as np
 import pandas as pd
-import seaborn as sns
+#import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
